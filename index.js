f1018a90a342b73c40627bb883ee2bc019334418
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-13 12:59:54 +0300

    complete array filter method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers,searchString){
+    return drivers.filter(driver => driver.toLowerCase() === searchString.toLowerCase())
+}
+function fuzzyMatch(drivers,searchString){
+    return drivers.filter(driver => driver.startsWith(searchString))
+}
+function matchName(drivers,searchString){
+    return drivers.filter(driver => driver.name === searchString)
+}
